1bc4b26bc02ccc0becfaec6e18bc1dbaf4525f84
SergioPastel <serginhocls@gmail.com>
2023-12-05 11:01:08 +0000

    Project folder added
    Moved projects repository Visual Studio's Repos folder into Github's folder.

---
+++ ProjetoReparacoes/ProjetoReparacoes/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ProjetoReparacoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCriarTabelas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCriarTabelas
+            // 
+            this.btnCriarTabelas.Location = new System.Drawing.Point(36, 68);
+            this.btnCriarTabelas.Name = "btnCriarTabelas";
+            this.btnCriarTabelas.Size = new System.Drawing.Size(75, 23);
+            this.btnCriarTabelas.TabIndex = 0;
+            this.btnCriarTabelas.Text = "Criar tabelas";
+            this.btnCriarTabelas.UseVisualStyleBackColor = true;
+//            this.btnCriarTabelas.Click += new System.EventHandler(this.btnCriarTabelas_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCriarTabelas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCriarTabelas;
+    }
+}
+
---
+++ ProjetoReparacoes/ProjetoReparacoes/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProjetoReparacoes
+{
+    public partial class Form1 : Form
+    {
+        // Codigo da conexão a base de dados confiavel
+        static string str = "Server=localhost;Database=ReparacoesDB;Trusted_Connection=True;";
+        SqlConnection conn = new SqlConnection(str);
+        public Form1()
+        {
+            InitializeComponent();
+            // Tentar conectar a base de dados
+            try 
+            {
+                conn.Open();
+                MessageBox.Show("Conexão a Base de Dados estabelecida com sucesso!");
+                conn.Close();
+            } 
+            catch (SqlException ex) 
+            {
+                   MessageBox.Show(ex.Message);
+            }            
+        }
+
+        /*private void btnCriarTabelas_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd;
+            string criarTbl = "CREATE TABLE"
+        }*/
+    }
+}
