b8b6c430479c32d5d024ee30e4a3f27a8800bcdb
SergioPastel <serginhocls@gmail.com>
2023-12-15 18:38:53 +0000

    Creates ReparosDB if not exists.
    The program will create the database when it runs for the first time(project settings).
    With this the program is effectively done.

--- ProjetoReparacoes/ProjetoReparacoes/Form1.cs
+++ ProjetoReparacoes/ProjetoReparacoes/Form1.cs
@@ -12,7 +12,9 @@ using System.Data.SqlClient;
 namespace ProjetoReparacoes
 {
     public partial class Form1 : Form
-    {       
+    {
+        const string str = "Server=localhost;Trusted_Connection=True;";
+        SqlConnection conn = new SqlConnection(str);
         public Form1()
         {
             InitializeComponent();            
@@ -35,6 +37,34 @@ namespace ProjetoReparacoes
         {
             Form frm = new FrmConcluidos();
             frm.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string CriarDb = "CREATE DATABASE IF NOT EXISTS ReparacoesDB;";
+            SqlCommand cmd = new SqlCommand(CriarDb, conn);
+            string usarDb = "USE ReparacoesDB; CREATE TABLE Clientes( id int NOT NULL IDENTITY (1,1), nome VARCHAR(30) NOT NULL, contacto VARCHAR(30) NOT NULL, PRIMARY KEY(id));CREATE TABLE ReparosAtivos( id INT NOT NULL IDENTITY (1,1), descricao VARCHAR(30), numSerie VARCHAR(30), avaria VARCHAR(500), idCliente INT NOT NULL FOREIGN KEY REFERENCES Clientes(id),\tcontacto VARCHAR(30), dtEntrada DATE, PRIMARY KEY(id));create table ReparosConcluidos (\tid INT NOT NULL,\tdescricao VARCHAR(30),\tnumSerie VARCHAR(30),\t\tavaria VARCHAR(500),\tidCliente INT NOT NULL,\tcontacto VARCHAR(30),\tdtEntrada DATE,\tdtReparacao DATE,\tdtEntrega DATE,\tterceirizado BIT NOT NULL,\treparador VARCHAR(30),\tPRIMARY KEY(id));";
+            try
+            {
+                conn.Open();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            if ((bool)Properties.Settings.Default["FirstRun"] == true)
+            {
+                //First application run
+                //Update setting
+                Properties.Settings.Default["FirstRun"] = false;
+                //Save setting
+                Properties.Settings.Default.Save();
+                //Create new instance of Dialog you want to show                
+                cmd = new SqlCommand(usarDb, conn);
+                cmd.ExecuteNonQuery();
+            }                       
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
         }
     }
 }
